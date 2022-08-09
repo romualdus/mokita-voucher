1b51b0905af41cfe0f61469662125c38d7346f4a
Aleph-WHV29 <aleph@Aleph-WHV29s-MacBook-Pro.local>
2022-08-09 14:09:20 +0700

    init

---
+++ scripts.js
@@ -0,0 +1,50 @@
+function toggleList() {
+  const listElement = document.getElementById('voucherList')
+  listElement.classList.toggle('hide')
+}
+
+function customSelectOption(e) {
+  const voucherIdElement = document.getElementById('voucherId')
+  const currentVoucherId = voucherIdElement.value
+  const voucherId = e.attributes['data-voucherId'].value
+  const voucherTitle = e.firstElementChild.textContent
+
+  voucherIdElement.value = voucherId
+  document.getElementById('voucherTitle').value = voucherTitle
+
+  if(currentVoucherId !== "") {
+    document.querySelector(`[data-voucherId='${currentVoucherId}']`).classList.remove('selected')
+  }
+
+  e.classList.add('selected')
+}
+
+function onYearChange(e) {
+  const year = e.value
+  const typeElement = document.getElementById('typeField')
+  typeElement.value = '<option value="" disabled selected>Loading...<option>'
+
+  fetch(`getBike.json`, {
+    method: 'GET'
+  })
+  .then(res =>  res.json())
+  .then(result => {
+    if(result.data) {
+      renderTypeOptions(result.data.rows)
+    }
+  })
+  .catch(error => {
+    typeElement.innerHTML = '<option value="" disabled selected>-- Pilih Tipe Motor --</option>'
+    console.error(error)}
+  );
+}
+
+function renderTypeOptions(data) {
+  const typeElement = document.getElementById('typeField')
+  typeElement.innerHTML = '<option value="" disabled selected>-- Pilih Tipe Motor --</option>'
+
+  data.map(option => {
+    typeElement.innerHTML += 
+      `<option value="${option.id}">${option.name}</option>`
+  })
+}
